740aab5cdf4441a786caec9d6173352b6e4958e4
“afpbondoc” <afpbondoc@gmail.com>
2022-12-22 03:07:40 +0100

    modify temp factmarkethistory

--- create_dim_tables/master_populate_factmarkethistory.sql
+++ create_dim_tables/master_populate_factmarkethistory.sql
@@ -53,15 +53,52 @@ limit 10
 
 /*MANO MANO
 
-select 
+SELECT *, MAX(DM_HIGH) OVER(rolling_1yr_window) AS FiftyTwoWeekHigh
+FROM staging.daily_market
+where DM_S_SYMB='AAAAAAAAAAAABOY'
+WINDOW rolling_1yr_window AS (    
+  PARTITION BY DM_S_SYMB 
+  ORDER BY DATE_DIFF(DM_DATE, '1970-01-01', YEAR)
+  RANGE BETWEEN CURRENT ROW AND 0 FOLLOWING 
+)
+;
+-------------------------------------------------------------------------
+
+with merged as 
+(select 
 dm1.DM_S_SYMB,
-dm1.DM_DATE, dm2.DM_DATE,
-dm1.DM_HIGH, dm2.DM_HIGH
+dm1.DM_DATE as DM_DATE1, dm2.DM_DATE as DM_DATE2,
+dm1.DM_HIGH as DM_HIGH1, dm2.DM_HIGH as DM_HIGH2
 -- max(DM_HIGH) over (partition by DM_S_SYMB order by DM_DATE rows between 365 preceding and current row) FiftyTwoWeekHigh
 from staging.daily_market dm1 
 join staging.daily_market dm2 on dm1.DM_S_SYMB=dm2.DM_S_SYMB
-where dm2.DM_DATE <= dm1.DM_DATE and dm2.DM_DATE> DATE_SUB(dm1.DM_DATE, INTERVAL 1 YEAR)
-order by dm1.DM_S_SYMB, dm1.DM_DATE
+where dm2.DM_DATE <= dm1.DM_DATE and dm2.DM_DATE> DATE_SUB(dm1.DM_DATE, INTERVAL 1 YEAR))
+
+select * from
+(
+select *, row_number() over(partition by DM_S_SYMB, DM_DATE1 order by DM_HIGH) as rn
+from merged
+)a where rn=1;
+;
+
+-- TO ADD: If there are no earnings for this company, NULL is assigned to PERatio and an alert condition is raised as described below.
+select CompanyName, Year, Quarter, QtrStartDate, sum(EPS) as EPS
+from staging.finwire_fin_record 
+where RecType='FIN'
+group by CompanyName, Year, Quarter, QtrStartDate
 limit 10;
+-------------------------------------------------------------------------
+-- TESTING
+
+SELECT * -- max(DM_HIGH) as max_high, count(*) as cnt, max(DM_DATE)as max_date, min(DM_DATE) as min_date
+FROM staging.daily_market
+where DM_DATE>'2016-02-06' and DM_DATE<='2017-02-06' and DM_S_SYMB='AAAAAAAAAAAABOY';
+
+-------------------------------------------------------------------------
+
+with quarterly as (
+  select
+)
 
 */
+
